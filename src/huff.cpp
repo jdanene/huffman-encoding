3860f222e5fae839c62b0f2490d89e3679280ce0
Puno.Lee <punolee@outlook.com>
2018-12-01 18:13:30 -0600

    can generate a new file

--- src/huff.cpp
+++ src/huff.cpp
@@ -11,6 +11,7 @@
 #define DEFAULT_OUTFILE "test-files/hamlet-ascii.txt.puff"
 
 using namespace ipd;
+std::map<char, std::string> bitcode;
 std::map<char, int> freq;
 struct huffmannode
 {
@@ -32,14 +33,17 @@ struct compare
         return (left->freq > right->freq);
     }
 };
-void printCodes(struct huffmannode *root, std::string str)
+
+void storecodes(struct huffmannode *root, std::string str)
 {
-    if (!root)
+    if (root == NULL)
+    {
         return;
+    }
     if (root->c != '#')
-        std::cout << root->c << ": " << str << std::endl;
-    printCodes(root->left, str + "0");
-    printCodes(root->right, str + "1");
+        bitcode[root->c] = str;
+    storecodes(root->left, str + "0");
+    storecodes(root->right, str + "1");
 }
 void createHuffmantree()
 {
@@ -61,7 +65,7 @@ void createHuffmantree()
         top->right = right;
         q.push(top);
     }
-    printCodes(q.top(), "");
+    storecodes(q.top(), "");
 }
 void buildfreqtable(std::istream &in)
 {
@@ -71,12 +75,32 @@ void buildfreqtable(std::istream &in)
         freq[c]++;
     }
 }
+void writecodes(std::istream &in, bostream &out)
+{
+    char c;
+    in.clear();
+    in.seekg(0, std::ios::beg);
+    while (in.read(&c, 1))
+    {
+        std::string code = bitcode[c];
+        for (int i = 0; i < code.size(); i++)
+        {
+            out.write_bits(code.at(i), 8);
+        }
+    }
+}
 void writefreqtabletofile(bostream &out)
 {
     for (auto v = freq.begin(); v != freq.end(); v++)
     {
-        //write to out file;
+        char ch = v->first;
+        char fq = '0' + v->second;
+        out.write_bits(ch, 8);
+        out.write_bits(':', 8);
+        out.write_bits(fq, 8);
+        out.write_bits(';', 8);
     }
+    out.write_bits('#', 8);
 }
 int main(int argc, const char *argv[])
 {
@@ -92,7 +116,10 @@ int main(int argc, const char *argv[])
     assert_good(out, argv);
 
     buildfreqtable(in);
+    writefreqtabletofile(out);
+
     createHuffmantree();
+    writecodes(in, out);
 
     return 0;
 }
